b478901536676f6abf3eabd379006216392933aa
Angela <angelamc999@outlook.com>
2019-08-17 20:45:20 +0800

    Beginning work

---
+++ Week5-task1-Ang.py
@@ -0,0 +1,10 @@
+# Week 5 Task 1
+
+# Your task is to implement the algorithms to calculate and validate check digits for different kinds of barcodes.
+
+# You must calculate and validate the check digit for:
+
+# - Credit Cards
+
+# - GTIN-13 Product Codes
+
