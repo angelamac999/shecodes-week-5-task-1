b57d2e03e8eba15e87b01af487a6019b0598a2b9
Angela <angelamc999@outlook.com>
2019-08-17 23:21:00 +0800

    Added credit card function attempt

--- Week5-task1-Ang.py
+++ Week5-task1-Ang.py
@@ -8,3 +8,44 @@
 
 # - GTIN-13 Product Codes
 
+
+evennum= 0
+oddnum =0
+evensum = []
+oddsum = []
+doubleevennum = []
+
+
+
+def checkcc(string):
+    for indexitem in range(len(ccnumberstring)):
+        if indexitem %2 ==0:
+            evennum =int(ccnumberstring[indexitem]) *2
+            if evennum > 9:
+                doubleevennum.append(evennum)
+                evenadd= sum(doubleevennum) # problem - this is adding up all the numbers over 9 
+                evensum.append(evenadd)
+            else:
+                evensum.append(evennum)
+        else:
+            oddnum = int(ccnumberstring[indexitem])
+            oddsum.append(oddnum)
+        
+    print('this is the evensum', evensum)
+    print('this is the odd sum', oddsum)
+    print('this is the doubleevennum', doubleevennum)
+
+    totaleven = sum(evensum)
+    totalodd = sum(oddsum)
+    total = totaleven + totalodd
+
+    if total %10 == 0:
+        print ('This is a valid credit card')
+    else:
+        print ("This is not a valid credit card")
+
+ccnumberstring = input('enter the credit card you would like to check, ')
+checkcc("ccnumberstring")
+
+
+
